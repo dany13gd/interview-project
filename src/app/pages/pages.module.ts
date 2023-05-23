d9e88d92b161329879ade9b1abd7364512cd98bd
dany13gd <dany13gd@gmail.com>
2023-05-23 14:29:55 -0600

    add ngx-bootstrap-icons

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, RouterModule, SharedModule],
+  imports: [CommonModule, RouterModule, SharedModule, MatIconModule],
   exports: [HomeComponent],
 })
 export class PagesModule {}
