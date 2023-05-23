fdf047b7cf70328fd6b7a847119f544271c12bfa
dany13gd <dany13gd@gmail.com>
2023-05-23 12:03:10 -0600

    create header

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent {
+  public btnUrl: string = '/devis';
+  public btnLabel: string = 'Demandez un Devis';
+  public openMenu = false;
+  public menuItems: any[] = [
+    {
+      title: 'Nos Prix',
+      path: '/nos-prix',
+    },
+    {
+      title: 'Avantages',
+      path: '/avantages',
+    },
+    {
+      title: 'Assurances',
+      path: '/assurances',
+    },
+    {
+      title: 'Avis Clients',
+      path: '/avis',
+    },
+    {
+      title: 'Blog',
+      path: '/',
+    },
+    {
+      title: 'FAQ',
+      path: '/',
+    },
+    {
+      title: 'Contact',
+      path: '/contact',
+    },
+    {
+      title: 'Conditions Générales',
+      path: '/conditions-generales',
+    },
+  ];
+  public toggleNav(): void {
+    this.openMenu = !this.openMenu;
+  }
+}
