5e7bae42ebb56429c45dd1915f625a28a0487432
dany13gd <dany13gd@gmail.com>
2023-05-23 12:49:21 -0600

    create home template and content-image

---
+++ src/app/shared/components/content-image/content-image.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-image',
+  templateUrl: './content-image.component.html',
+  styleUrls: ['./content-image.component.scss']
+})
+export class ContentImageComponent {
+
+}
