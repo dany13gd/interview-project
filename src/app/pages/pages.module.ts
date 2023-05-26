9f12e9cdf74bb655b837dbf059643d2117f6e0df
dany13gd <dany13gd@gmail.com>
2023-05-26 13:00:44 -0600

    create insurance template and accordion template

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,9 +6,10 @@ import { SharedModule } from '../shared/shared.module';
 import { MatIconModule } from '@angular/material/icon';
 import { PricesComponent } from './prices/prices.component';
 import { BenefitsComponent } from './benefits/benefits.component';
+import { InsurancesComponent } from './insurances/insurances.component';
 
 @NgModule({
-  declarations: [HomeComponent, PricesComponent, BenefitsComponent],
+  declarations: [HomeComponent, PricesComponent, BenefitsComponent, InsurancesComponent],
   imports: [CommonModule, RouterModule, SharedModule, MatIconModule],
   exports: [HomeComponent],
 })
