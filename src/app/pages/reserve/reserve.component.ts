42d8c7f69a7112c1c0fbebd34447ad9df860709e
dany13gd <dany13gd@gmail.com>
2023-05-27 13:40:09 -0600

    create reserve template

---
+++ src/app/pages/reserve/reserve.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Car } from 'src/app/shared/models/car';
+
+@Component({
+  selector: 'app-reserve',
+  templateUrl: './reserve.component.html',
+  styleUrls: ['./reserve.component.scss'],
+})
+export class ReserveComponent {}
